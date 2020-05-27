d95acdd95c9ef3f0fd89b54e1f1c207dc0898149
Amilcar Erazo <ajose.erazo@gmail.com>
2020-05-26 23:19:50 -0500

    Fix

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "4bd2a0e37981868a79163ece67256bef"
+    "revision": "d851163ecf55eda29314ba45c671585a"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });

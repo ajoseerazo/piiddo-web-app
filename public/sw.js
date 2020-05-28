d069188a93b1b1cf15c5d9c4fa5a506fd5adc88c
Amilcar Erazo <ajose.erazo@gmail.com>
2020-05-28 00:09:05 -0500

    Fixing

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "79726019d54245efc43e10e6d2f4503d"
+    "revision": "db1fde61c15591e06311862908fe98ad"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
--- src/pages/eta/styled.js
+++ src/pages/eta/styled.js
@@ -7,3 +7,11 @@ export const EtaWrapper = styled.div`
     margin-top: 50px;
   }
 `;
+
+export const MapWrapperStyled = styled.div`
+  height: calc(100vh - 70px);
+
+  @media screen and (max-width: 768px) {
+    height: calc(100vh - 50px);
+  }
+`;

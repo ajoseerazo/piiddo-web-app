4ac9a0c0e15687f386a07100688570070a2527aa
Amilcar Erazo <ajose.erazo@gmail.com>
2020-05-29 12:01:26 -0500

    Removing

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "6f5988f611d420b68f0fa0debb720e5f"
+    "revision": "fe3659a001ac67aede24eb2bcc91069b"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });

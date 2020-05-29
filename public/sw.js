128db2ff3e156c6225bdc810ca7c5151738d5760
Amilcar Erazo <ajose.erazo@gmail.com>
2020-05-29 10:25:50 -0500

    Adding robots.txt

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "b9ac9e4af5052e8d460626e26e51974c"
+    "revision": "de7a32d1548bef80763663d45f44d55b"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });

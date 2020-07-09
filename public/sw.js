c00fac6cfd1d957a43635084561b982af9a7835c
Amilcar Erazo <ajose.erazo@gmail.com>
2020-07-08 23:11:00 -0500

    .

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "2525850b5780f7188960a195769ac9cc"
+    "revision": "7c03a5600466fae3a818cdb26e52b282"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });

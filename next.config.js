ffb965a8cc1372d0d5d36e1cb26b285f38af005c
Amilcar Erazo <ajose.erazo@gmail.com>
2020-07-12 21:45:19 -0500

    Purging unused

--- next.config.js
+++ next.config.js
@@ -1,34 +1,32 @@
 const withSass = require("@zeit/next-sass");
 const withPWA = require("next-pwa");
 const withCSS = require("@zeit/next-css");
-const withPurgeCss = require("next-purgecss");
+// const withPurgeCss = require('next-purgecss')
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = withPurgeCss(
-  withCSS(
-    withSass(
-      withPWA({
-        pwa: {
-          // disable: process.env.NODE_ENV === "production" ? false : true,
-          dest: "public",
-        },
-        webpack(config, options) {
-          config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+module.exports = withCSS(
+  withSass(
+    withPWA({
+      pwa: {
+        // disable: process.env.NODE_ENV === "production" ? false : true,
+        dest: "public",
+      },
+      webpack(config, options) {
+        config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
 
-          config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-              loader: "url-loader",
-              options: {
-                limit: 100000,
-                name: "[name].[ext]",
-              },
-            },
-          });
+        config.module.rules.push({
+          test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+              name: '[name].[ext]'
+            }
+          }
+        })
 
-          return config;
-        },
-      })
-    )
+        return config;
+      }
+    })
   )
 );

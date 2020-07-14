5dc41d2615dce40000b50e9e2aeab7b6139fd611
Amilcar Erazo <ajose.erazo@gmail.com>
2020-07-14 15:05:32 -0500

    adding compralo en colomia

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "482650d76cb031e49f3236d9c199c05c"
+    "revision": "59f1141ade61ae6f154038b2b943e886"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });

4d7e7437a01f78c5fd8c99500ee0b93a46fd2a4b
Amilcar Erazo <ajose.erazo@gmail.com>
2020-07-18 12:16:42 -0500

    Showing eta

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "b8cf96f972807a92c09686b4d7593b2c"
+    "revision": "ce5493bb5e2cdc178d24675b01b2a699"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });

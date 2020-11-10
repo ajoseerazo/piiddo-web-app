6fb131d20cd97fdeb56d53723650155135dd2230
Amilcar Erazo <ajose.erazo@gmail.com>
2020-11-10 13:38:33 -0500

    changing dollar

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -797,7 +797,7 @@ const CheckoutPage = ({
                     <CheckoutTotalPrice>
                       Bs{" "}
                       {`${new Intl.NumberFormat("es").format(
-                        finalAmount * 550000
+                        finalAmount * 600000
                       )}`}
                     </CheckoutTotalPrice>
                   </div>
@@ -854,7 +854,7 @@ const CheckoutPage = ({
               amount={
                 paymentMethodSelected.value === "zelle"
                   ? finalAmount
-                  : finalAmount * 550000
+                  : finalAmount * 600000
               }
               orderId={order.id}
               onFinishPayment={onFinishPayment}

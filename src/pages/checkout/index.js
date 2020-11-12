4a5dd6ca675827da87a517501cec69eaab1a3bfc
Amilcar Erazo <ajose.erazo@gmail.com>
2020-11-12 12:29:00 -0500

    Fix

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -797,7 +797,7 @@ const CheckoutPage = ({
                     <CheckoutTotalPrice>
                       Bs{" "}
                       {`${new Intl.NumberFormat("es").format(
-                        finalAmount * 600000
+                        finalAmount * 700000
                       )}`}
                     </CheckoutTotalPrice>
                   </div>
@@ -854,7 +854,7 @@ const CheckoutPage = ({
               amount={
                 paymentMethodSelected.value === "zelle"
                   ? finalAmount
-                  : finalAmount * 600000
+                  : finalAmount * 700000
               }
               orderId={order.id}
               onFinishPayment={onFinishPayment}

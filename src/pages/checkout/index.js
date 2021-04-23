f860aaee03b211d4c98c8a3049f815c4ac2b9af9
Amilcar Erazo <ajose.erazo@gmail.com>
2021-04-23 08:27:23 -0500

    Changing dollar

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -60,7 +60,7 @@ const { getCoupon } = couponsActions;
 
 const DASH_DISCOUNT = 0.1;
 
-const DOLLAR_BASE = 2000000;
+const DOLLAR_BASE = 2750000;
 const DOLLAR_PRICE = DOLLAR_BASE + DOLLAR_BASE * 0.1;
 
 const CheckoutPage = ({

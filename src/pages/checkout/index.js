8b3f16af384b2a5b6abde9dcc3f2c2db100b945a
Amilcar Erazo <ajose.erazo@gmail.com>
2021-07-23 09:28:33 -0500

    Fixing dollar

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -60,7 +60,7 @@ const { getCoupon } = couponsActions;
 
 const DASH_DISCOUNT = 0.1;
 
-const DOLLAR_BASE = 4011743;
+const DOLLAR_BASE = 4500000;
 const DOLLAR_PRICE = DOLLAR_BASE + DOLLAR_BASE * 0;
 
 const CheckoutPage = ({
